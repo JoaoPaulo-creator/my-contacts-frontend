0d3427469558975dce2fb372dbc2ef01e60a9f7c
JoaoPaulo-creator <joaosarto@gmail.com>
2022-12-01 12:23:44 -0300

    Criado componente button, select e input

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,35 @@
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input'
+import Select from '../../components/Select'
+import Button from '../../components/Button'
+
+
+
 
 export default function NewContact(){
     return (
-        <PageHeader
+        <>
+            <PageHeader
             title='Novo contato'
-        />
+            />
+            <Input placeholder='Nome'/>
+            <Select>
+                <option value='123'>Instagram</option>
+                <option value='123'>LinkdIn</option>
+                <option value='123'>Twitter</option>
+                <option value='123'>Telegram</option>
+                <option value='123'>Instagram</option>
+                <option value='123'>Instagram</option>
+            </Select>
+
+            <Button type='button'>
+                Salvar contato
+            </Button>
+
+            <Button type='button' disabled>
+                Teste
+            </Button>
+
+        </>
     )
 }
